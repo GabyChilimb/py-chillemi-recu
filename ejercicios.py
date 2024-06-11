b0d66882714703641ad10e23fc923f79d01f0279
GabyChilimb <163897636+GabyChilimb@users.noreply.github.com>
2024-06-11 18:28:32 -0300

    Tp

--- ejercicios.py
+++ ejercicios.py
@@ -53,4 +53,85 @@ def Punto5():
     EnSegundos = resultado1 + resultado2 + segundos
 
     print("El resultado expresado en segundos es: "+ EnSegundos)
-    Punto5()
+    #Punto5
+
+def Punto6():
+    ladoA = int(input("Cuanto mide el lado a?"))
+    ladoB = int(input("Cuanto mide el lado b?"))
+
+
+    suptriangulo = ladoA * ladoB /2
+    print("La superficie del triangulo es: "+suptriangulo)
+#Punto6()
+
+def Punto7():
+    n1 = input("Ponga un número.")
+    n1 = int(n1)
+    n2 = input("Ponga un segundo número.")
+    n2 = int(n2)
+    n3 = input("Ponga un tercer número.")
+    n3 = int(n3)
+    n4 = input("Ponga un cuarto número.")
+    n4 = int(n4)
+    n5 = input("Ponga un quinto número.")
+    n5 = int(n5)
+    n6 = input("Ponga el último número.")
+    n6 = int(n6)
+
+    total = n1+ n2 + n3 + n4 + n5 + n6
+
+    promedio = total/6
+
+    print("El promedio es: "+ promedio)
+#Punto7()
+
+
+def Punto8():
+    n1 = int(input("Ponga un número."))
+    n2 = int(input("Ponga otro número."))
+
+    numero1 = n1/100
+    porcentaje = n2 / numero1
+
+    print("El promedio es:%", porcentaje)
+#Punto8()
+
+def Punto9():
+    fecha = int(input("Ingrese la fecha en formato DDMMAAAA."))
+
+    dia = fecha//1000000
+    mes = (fecha//10000) % 100
+    año = fecha % 10000
+
+    print("Día: ", dia)
+    print("Mes: ", mes)
+    print("Año: ", año)
+#Punto9()
+
+def Punto10():
+    a = int(input("Ingrese la calificación de exámen parcial."))
+    b = int(input("Ingrese la calificación por TPs."))
+    c = int(input("Ingrese la calificación del exámen integrador."))
+
+    calificacionA = a * 0.3
+    calificacionB = b * 0.2
+    calificacionC = c * 0.5
+
+    notafinal = calificacionA + calificacionB + calificacionC
+    print("El promedio del estudiante es: "+ notafinal)
+#Punto10
+
+def Punto11():
+
+    autos = []
+    inputUser = ""
+    while inputUser != "SALIR":
+        inputUser = input("ingrese el valor del auto vendido o SALIR")
+        if inputUser != "SALIR":
+            inputUser = float(inputUser)
+            autos.append(inputUser)
+
+    AUx = sum(autos)
+    comision = AUx * 0.05
+
+#Punto11()
